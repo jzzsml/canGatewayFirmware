a3f1f14210dc8713dca7c837e485bbac9f1ac8a4
jzzsml pcb-pc <jsserg24@gmail.com>
2019-07-25 12:30:47 +0300

    can example is added

--- source/main.c
+++ source/main.c
@@ -1,63 +1,257 @@
-    /***************************************************************************************************
-                                   ExploreEmbedded    
- ****************************************************************************************************
- * File:   gpio.h
- * Version: 15.0
- * Author: ExploreEmbedded
- * Website: http://www.exploreembedded.com/wiki
- * Description: File contains the Program to demonstrate Led_Blinging using ARM Gcc for lpc1768
+/**********************************************************************
+* $Id$		can_test_bypass_mode.c			2010-05-21
+*//**
+* @file		can_test_bypass_mode.c
+* @brief	This example used to test Bypass mode
+* @version	2.0
+* @date		21. May. 2010
+* @author	NXP MCU SW Application Team
+*
+* Copyright(C) 2010, NXP Semiconductor
+* All rights reserved.
+*
+***********************************************************************
+* Software that is described herein is for illustrative purposes only
+* which provides customers with programming information regarding the
+* products. This software is supplied "AS IS" without any warranties.
+* NXP Semiconductors assumes no responsibility or liability for the
+* use of the software, conveys no license or title under any patent,
+* copyright, or mask work right to the product. NXP Semiconductors
+* reserves the right to make changes in the software without
+* notification. NXP Semiconductors also make no representation or
+* warranty that such application will be suitable for the specified
+* use without further testing or modification.
+**********************************************************************/
+#include "lpc17xx_can.h"
+#include "lpc17xx_libcfg.h"
+#include "lpc17xx_pinsel.h"
+#include "debug_frmwrk.h"
 
-The libraries have been tested on ExploreEmbedded development boards. We strongly believe that the 
-library works on any of development boards for respective controllers. However, ExploreEmbedded 
-disclaims any kind of hardware failure resulting out of usage of libraries, directly or indirectly.
-Files may be subject to change without prior notice. The revision history contains the information 
-related to updates. 
+/* Example group ----------------------------------------------------------- */
+/** @defgroup CAN_test_bypass_mode	CAN_test_bypass_mode
+ * @ingroup CAN_Examples
+ * @{
+ */
+/************************** PRIVATE VARIABLES *************************/
+uint8_t menu[]=
+	"*******************************************************************************\n\r"
+	"Hello NXP Semiconductors \n\r"
+	"CAN bypass demo \n\r"
+	"\t - MCU: LPC17xx \n\r"
+	"\t - Core: ARM CORTEX-M3 \n\r"
+	"\t - Communicate via: UART0 - 115200 bps \n\r"
+	"Use two CAN peripherals: CAN1 and CAN2 to communicate\n\r"
+	"This example used to test Bypass mode\n\r"
+	"*******************************************************************************\n\r";
+
+/** CAN variable definition **/
+CAN_MSG_Type TXMsg, RXMsg; // messages for test Bypass mode
+uint32_t CANRxCount, CANTxCount = 0;
+
+/************************** PRIVATE FUNCTIONS *************************/
+void CAN_IRQHandler(void);
+
+void CAN_InitMessage(void);
+void PrintMessage(CAN_MSG_Type* msg);
+void print_menu();
 
 
-GNU GENERAL PUBLIC LICENSE: 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-
-Errors and omissions should be reported to codelibraries@exploreembedded.com
- **************************************************************************************************/
- 
-/*-----------------------------------------------------------------------------
-                 Program to demonstrate Led_Blinging
-  -----------------------------------------------------------------------------
- 
-note: Leds can be connected to any of the ports as all the ports are blinked
-
-------------------------------------------------------------------------------*/
-/*
-#include "lpc17xx.h"    
-#include "delay.h"     //User defined library which conatins the delay routines
-   */           
-/* start the main program */
-void main() 
+/*----------------- INTERRUPT SERVICE ROUTINES --------------------------*/
+/*********************************************************************//**
+ * @brief		CAN_IRQ Handler, control receive message operation
+ * param[in]	none
+ * @return 		none
+ **********************************************************************/
+void CAN_IRQHandler()
 {
-   /* SystemInit();                    //Clock and PLL configuration 
-    LPC_PINCON->PINSEL4 = 0x000000; //Configure the Pins for GPIO;
-    LPC_GPIO2->FIODIR = 0xffffffff; //Configure the PORT pins as OUTPUT;
-*/
-  while(1)
-    {
+	uint8_t IntStatus;
+	uint32_t data1;
+	/* get interrupt status
+	 * Note that: Interrupt register CANICR will be reset after read.
+	 * So function "CAN_IntGetStatus" should be call only one time
+	 */
+	IntStatus = CAN_IntGetStatus(LPC_CAN2);
+	//check receive interrupt
+	if((IntStatus>>0)&0x01)
+	{
+		CAN_ReceiveMsg(LPC_CAN2,&RXMsg);
+		PrintMessage(&RXMsg);
+		CANRxCount++; //count success received message
+		//increase data for next TX Message
+		TXMsg.id ++;
+		data1 = (TXMsg.dataA[0])|(((TXMsg.dataA[1]))<<8)|((TXMsg.dataA[2])<<16)|((TXMsg.dataA[3])<<24);
+		if(data1 == 0xFFFFFFFF) data1 = 0;
+		else data1++;
+		*((uint8_t *) &TXMsg.dataA[0])= *((uint8_t *) &TXMsg.dataB[0])= data1 & 0x000000FF;
+		*((uint8_t *) &TXMsg.dataA[1])= *((uint8_t *) &TXMsg.dataB[1])=(data1 & 0x0000FF00)>>8;;
+		*((uint8_t *) &TXMsg.dataA[2])= *((uint8_t *) &TXMsg.dataB[2])=(data1 & 0x00FF0000)>>16;
+		*((uint8_t *) &TXMsg.dataA[3])= *((uint8_t *) &TXMsg.dataB[3])=(data1 & 0xFF000000)>>24;
 
-     /* Turn On all the leds and wait for one second */ 
-   /*    LPC_GPIO2->FIOSET = 0xffffffff;     // Make all the Port pins as high  
-       DELAY_sec(1);
-
-
-       LPC_GPIO2->FIOCLR = 0xffffffff;     // Make all the Port pins as low  
-       DELAY_sec(1);
-   */ }
+		CAN_SendMsg(LPC_CAN1, &TXMsg);
+	}
 }
+
+/*-------------------------PRIVATE FUNCTIONS----------------------------*/
+/*********************************************************************//**
+ * @brief		Print Message via COM1
+ * param[in]	msg: point to CAN_MSG_Type object that will be printed
+ * @return 		none
+ **********************************************************************/
+void PrintMessage(CAN_MSG_Type* CAN_Msg)
+{
+	uint32_t data;
+	_DBG("Message ID:     ");
+	_DBH32(CAN_Msg->id);_DBG_("");
+	_DBG("Message length: ");
+	_DBH32(CAN_Msg->len);_DBG_(" BYTES");
+	_DBG("Message type:   ");
+	if(CAN_Msg->type==DATA_FRAME)
+	{
+		_DBG_("DATA FRAME ");
+	}
+	else
+		_DBG_("REMOTE FRAME ");
+	_DBG("Message format: ");
+	if(CAN_Msg->format==STD_ID_FORMAT)
+	{
+		_DBG_("STANDARD ID FRAME FORMAT");
+	}
+	else
+		_DBG_("EXTENDED ID FRAME FORMAT");
+	_DBG("Message dataA:  ");
+	data = (CAN_Msg->dataA[0])|(CAN_Msg->dataA[1]<<8)|(CAN_Msg->dataA[2]<<16)|(CAN_Msg->dataA[3]<<24);
+	_DBH32(data);_DBG_("");
+	data = (CAN_Msg->dataB[0])|(CAN_Msg->dataB[1]<<8)|(CAN_Msg->dataB[2]<<16)|(CAN_Msg->dataB[3]<<24);
+	_DBG("Message dataB:  ");
+	_DBH32(data);_DBG_("");
+	_DBG_("");
+}
+
+/*********************************************************************//**
+ * @brief		Initialize transmit and receive message for Bypass operation
+ * @param[in]	none
+ * @return 		none
+ **********************************************************************/
+void CAN_InitMessage(void) {
+	TXMsg.format = EXT_ID_FORMAT;
+	TXMsg.id = 0x00001234;
+	TXMsg.len = 8;
+	TXMsg.type = DATA_FRAME;
+	TXMsg.dataA[0] = TXMsg.dataA[1] = TXMsg.dataA[2] = TXMsg.dataA[3] = 0x00000000;
+	TXMsg.dataB[0] = TXMsg.dataB[1] = TXMsg.dataB[2] = TXMsg.dataB[3] = 0x00000000;
+
+	RXMsg.format = 0x00;
+	RXMsg.id = 0x00;
+	RXMsg.len = 0x00;
+	RXMsg.type = 0x00;
+	RXMsg.dataA[0] = RXMsg.dataA[1] = RXMsg.dataA[2] = RXMsg.dataA[3] = 0x00000000;
+	RXMsg.dataB[0] = RXMsg.dataB[1] = RXMsg.dataB[2] = RXMsg.dataB[3] = 0x00000000;
+}
+
+/*********************************************************************//**
+ * @brief		print menu
+ * @param[in]	none
+ * @return 		none
+ **********************************************************************/
+void print_menu()
+{
+	_DBG_(menu);
+}
+
+/*-------------------------MAIN FUNCTION------------------------------*/
+/*********************************************************************//**
+ * @brief		c_entry: Main CAN program body
+ * @param[in]	none
+ * @return 		int
+ **********************************************************************/
+int c_entry(void) { /* Main Program */
+	PINSEL_CFG_Type PinCfg;
+
+	/* Initialize debug via UART0
+	 * – 115200bps
+	 * – 8 data bit
+	 * – No parity
+	 * – 1 stop bit
+	 * – No flow control
+	 */
+	debug_frmwrk_init();
+	print_menu();
+
+	/* Pin configuration
+	 * CAN1: select P0.0 as RD1. P0.1 as TD1
+	 * CAN2: select P2.7 as RD2, P2.8 as RD2
+	 */
+	PinCfg.Funcnum = 1;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Pinnum = 0;
+	PinCfg.Portnum = 0;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 1;
+	PINSEL_ConfigPin(&PinCfg);
+
+	PinCfg.Pinnum = 7;
+	PinCfg.Portnum = 2;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 8;
+	PINSEL_ConfigPin(&PinCfg);
+
+	//Initialize CAN1 & CAN2
+	CAN_Init(LPC_CAN1, 125000);
+	CAN_Init(LPC_CAN2, 125000);
+
+	//Enable Interrupt
+	CAN_IRQCmd(LPC_CAN2, CANINT_RIE, ENABLE);
+
+	//Enable CAN Interrupt
+	NVIC_EnableIRQ(CAN_IRQn);
+
+	_DBG_("CAN test Bypass Mode function...");
+	_DBG_("Press '1' to initialize CAN message...");_DBG_("");
+	while(_DG !='1');
+	CAN_SetAFMode(LPC_CANAF,CAN_AccBP);
+	CAN_InitMessage();
+	PrintMessage(&TXMsg);
+	_DBG_("Message ID and data will be increased continuously...");
+
+	_DBG_("Press '2' to start CAN operation...");
+	while(_DG !='2');
+
+	/** To test Bypass Mode: we send infinite messages to CAN2 and check
+	 * receive process via COM1
+	 */
+	CAN_SendMsg(LPC_CAN1, &TXMsg);
+
+	while (1);
+}
+
+/* With ARM and GHS toolsets, the entry point is main() - this will
+ allow the linker to generate wrapper code to setup stacks, allocate
+ heap area, and initialize and copy code and data segments. For GNU
+ toolsets, the entry point is through __start() in the crt0_gnu.asm
+ file, and that startup code will setup stacks and data */
+int main(void) {
+	return c_entry();
+}
+
+#ifdef  DEBUG
+/*******************************************************************************
+ * @brief		Reports the name of the source file and the source line number
+ * 				where the CHECK_PARAM error has occurred.
+ * @param[in]	file Pointer to the source file name
+ * @param[in]    line assert_param error line source number
+ * @return		None
+ *******************************************************************************/
+void check_failed(uint8_t *file, uint32_t line) {
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+	/* Infinite loop */
+	while (1)
+		;
+}
+#endif
+
+/*
+ * @}
+ */
